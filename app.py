503ae14e8b828d935ecb5e5adf45b985a725aa39
toufani1515 <toufani1515@gmail.com>
2019-11-09 22:23:17 +0800

    tambah sumber daya

--- app.py
+++ app.py
@@ -1,63 +1,32 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "iniSecretKeyKu2019"
 
 
 @app.route("/")
-def indexku():
-    # belajar looping
-    hari = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"]
-    # conditioning / if-else
-    suasana = "sedih"  # jika senang maka dia juga cinta jika tidak, maka dia sedih
-    # set variabel
-    return render_template("index.html", value=hari, suasana=suasana)
-
-
-@app.route("/contact")
-def contactku():
-    return render_template("contact.html")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/about")
-def aboutku():
+def about():
     return render_template("about.html")
 
 
-# parsing nilai int, string
-@app.route("/parsing/<string:nilaiku>")
-def ayo_parsing(nilaiku):
-    return "nilainya adalah : {}".format(nilaiku)
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
-# argument parser
-@app.route("/parsingargument")
-def ayo_argument():
-    data = request.args.get("nilai")
-    return "nilainya dari argument parser adalah {}".format(data)
+@app.route("/redirect-about")
+def ayo_redirect_about():
+    return redirect(url_for("about"))
 
 
-# memparsing nilai dari url untuk mengeset nilai session
-@app.route("/halaman/<int:nilai>")
-def session_1(nilai):
-    session["nilaiku"] = nilai
-    return "Berhasil mengeset nilainya"
-
-
-@app.route("/halaman/lihat")
-def view_session_1():
-    try:  # try except ini sama dengan if else
-        data = session["nilaiku"]
-        return "Nilai yang telah diset adalah = {}".format(data)
-    except:
-        return "Anda tidak memiliki nilai session lagi"
-
-
-# logout / destroy session
-@app.route("/halaman/logout")
-def logout_session_1():
-    session.pop("nilaiku")
-    return "Berhasil logout / menghapus session"
+@app.route("/redirect-contact")
+def ayo_redirect_contact():
+    return redirect(url_for("contact"))
 
 
 if __name__ == "__main__":
